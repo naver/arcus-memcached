d8426ddb3de908e8ee41cbe52e6be8f16713ea2f
Park JunHyun <junhyun.park@jam2in.com>
2015-01-01 13:15:05 +0900

    Refactor: complete_nread() function.

--- memcached.c
+++ memcached.c
@@ -6915,14 +6915,14 @@ static void complete_nread_ascii(conn *c) {
     }
 }
 
-static void complete_nread(conn *c) {
+static void complete_nread(conn *c)
+{
     assert(c != NULL);
-    assert(c->protocol == ascii_prot
-           || c->protocol == binary_prot);
+    assert(c->protocol == ascii_prot || c->protocol == binary_prot);
 
     if (c->protocol == ascii_prot) {
         complete_nread_ascii(c);
-    } else if (c->protocol == binary_prot) {
+    } else {
         complete_nread_binary(c);
     }
 }
