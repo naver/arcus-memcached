2631eb2fd3d32b60321a942587d2ca12b88bcda2
Park JunHyun <junhyun.park@jam2in.com>
2016-06-30 20:29:20 +0900

    update CopyRight.

--- engines/default/assoc.h
+++ engines/default/assoc.h
@@ -1,7 +1,7 @@
 /*
  * arcus-memcached - Arcus memory cache server
  * Copyright 2010-2014 NAVER Corp.
- * Copyright 2015 JaM2in Co., Ltd.
+ * Copyright 2015-2016 JaM2in Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
