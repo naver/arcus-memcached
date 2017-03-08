7efd7585ce509e8b776fff25a39a7dedb7f97200
Park JunHyun <junhyun.park@jam2in.com>
2017-03-08 20:02:38 +0900

    Change the data type of nprefix: uint16_t => uint8_t.

--- engines/default/assoc.h
+++ engines/default/assoc.h
@@ -21,9 +21,9 @@
 typedef struct _prefix_t prefix_t;
 
 struct _prefix_t {
-    uint16_t nprefix; /* length of prefix name */
-    uint8_t  internal; /* internal prefix: 1 or 0 */
-    uint8_t  dummy8;
+    uint8_t  nprefix;  /* the length of prefix name */
+    uint8_t  internal; /* is internal prefix ? 1 or 0 */
+    uint16_t dummy16;
     uint32_t prefix_items;
     uint64_t list_hash_items;
     uint64_t set_hash_items;
--- engines/default/items.h
+++ engines/default/items.h
@@ -34,10 +34,10 @@ typedef struct _hash_item {
                          * Upper 8 bits are reserved for engine implementation.
                          */
     uint16_t nkey;      /* The total length of the key (in bytes) */
-    uint16_t nprefix;   /* The prefix length of the key (in bytes) */
-    uint16_t dummy16;
-    uint32_t hval;      /* hash value of key string */
     uint32_t nbytes;    /* The total length of the data (in bytes) */
+    uint8_t  nprefix;   /* The prefix length of the key (in bytes) : 0 ~ 250 */
+    uint8_t  dummy[3];
+    uint32_t hval;      /* hash value of key string */
 } hash_item;
 
 /* Item Internal Flags */
