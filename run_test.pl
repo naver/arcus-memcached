bf62382c034cf3338462dd375a765bd5d4078d71
jooho812 <kasin812@jam2in.com>
2018-04-27 16:48:12 +0900

    TEST : separate into big test and small test

--- run_test.pl
+++ run_test.pl
@@ -7,22 +7,20 @@ my $script_type = shift;
 my @engine_list = ("default");
 my $opt = '--job 5'; # --job N : run N test jobs in parallel
 my $srcdir = getcwd;
-my $ext = ".t";
+my $ext = "s";
 
 if ($script_type eq "big") {
-    $ext = "t";
+    $ext = "b";
 }
 
 # default engine specific test script : ./t/default
 # common engine test script : ./t
 
 if (grep $_ eq $engine_name, @engine_list) {
-    #system("prove $opt --ext '$ext' $srcdir/t/$engine_name $srcdir/t :: $engine_name");
-    system("prove $opt --ext '$ext' $srcdir/t :: $engine_name");
+    system("prove $opt - < $srcdir/t/tlist/engine_$engine_name\_$ext\.txt :: $engine_name");
 } elsif ("$engine_name" eq "") {
     # default engine test
-    #system("prove $opt --ext '$ext' $srcdir/t/default $srcdir/t :: default");
-    system("prove $opt --ext '$ext' $srcdir/t :: default");
+    system("prove $opt - < $srcdir/t/tlist/engine_default_$ext\.txt :: default");
 } else {
     system('echo -e \'\033[31mmake test [TYPE=<small || big>] [ENGINE=<default>]\033[0m\n\'');
     exit(1);
