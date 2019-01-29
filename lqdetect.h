d83e7f6412dc66ac3df912d01b558ad2bafcb8dc
dohee <ehgml961017@naver.com>
2019-01-29 10:25:18 +0900

    CLEANUP: refactoring at long query detection

--- lqdetect.h
+++ lqdetect.h
@@ -2,6 +2,7 @@
 #define LQDETECT_H
 
 #include "memcached/extension_loggers.h"
+#include "memcached/util.h"
 
 #define DETECT_LONG_QUERY
 #define LONGQ_STAT_STRLEN        300
@@ -55,8 +56,33 @@ char *lqdetect_buffer_get(int cmd, uint32_t *length, uint32_t *cmdcnt);
 void lqdetect_buffer_release(int bufcnt);
 int lqdetect_start(uint32_t lqdetect_base, bool *already_started);
 void lqdetect_stop(bool *already_stopped);
+#ifdef REFACTORING_LONG_QUERY
+void lqdetect_get_stats(char* str);
+bool lqdetect_lop_insert(char *client_ip, char *key, int coll_index);
+bool lqdetect_lop_delete(char *client_ip, char *key, uint32_t del_count,
+                         int32_t from_index, int32_t to_index, const int delete_or_drop);
+bool lqdetect_lop_get(char *client_ip, char *key, uint32_t elem_count,
+                      int32_t from_index, int32_t to_index, const int delete_or_drop);
+bool lqdetect_sop_get(char *client_ip, char *key, uint32_t elem_count,
+                      uint32_t count, const int delete_or_drop);
+bool lqdetect_mop_get(char *client_ip, char *key, uint32_t elem_count,
+                      uint32_t coll_numkeys, const int delete_or_drop);
+bool lqdetect_mop_delete(char *client_ip, char *key, uint32_t del_count,
+                         uint32_t coll_numkeys, const int delete_or_drop);
+bool lqdetect_bop_gbp(char *client_ip, char *key, uint32_t elem_count,
+                      uint32_t from_posi, uint32_t to_posi, int order);
+bool lqdetect_bop_get(char *client_ip, char *key, uint32_t access_count,
+                      const bkey_range *bkrange, const eflag_filter *efilter,
+                      uint32_t offset, uint32_t count, const int delete_or_drop);
+bool lqdetect_bop_count(char *client_ip, char *key, uint32_t access_count,
+                        const bkey_range *bkrange, const eflag_filter *efilter);
+bool lqdetect_bop_delete(char *client_ip, char *key, uint32_t access_count,
+                         const bkey_range *bkrange, const eflag_filter *efilter,
+                         uint32_t count, const int delete_or_drop);
+#else
 void lqdetect_stats(struct lq_detect_stats *stats);
 bool lqdetect_discriminant(uint32_t overhead);
-bool lqdetect_save_cmd(char client_ip[], char *key,
-                      enum lq_detect_command cmd, struct lq_detect_argument *arg);
+bool lqdetect_save_cmd(char client_ip[], char* key,
+                       enum lq_detect_command cmd, struct lq_detect_argument *arg);
+#endif
 #endif
