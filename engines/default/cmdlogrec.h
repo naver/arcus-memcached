dee3db32321d76c74e190cea17d96b2e75a843a5
MinWooJin <alsdn653@jam2in.com>
2019-08-14 15:37:30 +0900

    [persistence] generate command log of Collection operations

--- engines/default/cmdlogrec.h
+++ engines/default/cmdlogrec.h
@@ -31,7 +31,10 @@ enum log_type {
     LOG_MAP_ELEM_DELETE,
     LOG_BT_ELEM_INSERT,
     LOG_BT_ELEM_DELETE,
+#ifdef ENABLE_PERSISTENCE_03_CMDLOG_COLL
+#else
     LOG_BT_ELEM_ARITHMETIC,
+#endif
     LOG_SNAPSHOT_ELEM,
     LOG_SNAPSHOT_HEAD,
     LOG_SNAPSHOT_TAIL
@@ -178,12 +181,137 @@ typedef struct _snapshot_elem_log {
     char             *valptr;
 } SnapshotElemLog;
 
+#ifdef ENABLE_PERSISTENCE_03_CMDLOG_COLL
+/* List Elem Insert Log Record */
+typedef struct _List_elem_insert_data {
+    uint16_t keylen;  /* key length */
+    uint16_t reserved_16[1];
+    uint32_t vallen;  /* value length */
+    uint32_t totcnt;  /* total element count */
+    int32_t  eindex;  /* element index */
+    char     data[1];
+} ListElemInsData;
+
+typedef struct _List_elem_insert_log {
+    LogHdr          header;
+    ListElemInsData body;
+    char            *keyptr;
+    char            *valptr;
+} ListElemInsLog;
+
+/* List Elem Delete Log Record */
+typedef struct _List_elem_delete_data {
+    uint16_t keylen;  /* key length */
+    uint16_t reserved_16[1];
+    uint32_t totcnt;  /* total element count */
+    int32_t  eindex;  /* element index */
+    uint32_t delcnt;  /* delete count */
+    char     data[1];
+} ListElemDelData;
+
+typedef struct _List_elem_delete_log {
+    LogHdr          header;
+    ListElemDelData body;
+    char            *keyptr;
+} ListElemDelLog;
+
+/* Map Elem Insert Log Record */
+typedef struct _Map_elem_insert_data {
+    uint16_t keylen;  /* key length */
+    uint8_t  fldlen;  /* field length */
+    uint8_t  reserved_8[1];
+    uint32_t vallen;  /* value length */
+    char     data[1];
+} MapElemInsData;
+
+typedef struct _Map_elem_insert_log {
+    LogHdr         header;
+    MapElemInsData body;
+    char           *keyptr;
+    char           *datptr;
+} MapElemInsLog;
+
+/* Map Elem Delete Log Record */
+typedef struct _Map_elem_delete_data {
+    uint16_t keylen;  /* key length */
+    uint8_t  fldlen;  /* field length */
+    char     data[1];
+} MapElemDelData;
+
+typedef struct _Map_elem_delete_log {
+    LogHdr         header;
+    MapElemDelData body;
+    char           *keyptr;
+    char           *datptr;
+} MapElemDelLog;
+
+/* Set Elem Insert Log Record */
+typedef struct _Set_elem_insert_data {
+    uint16_t keylen;  /* key length */
+    uint16_t reserved_16[1];
+    uint32_t vallen;  /* value length */
+    char     data[1];
+} SetElemInsData;
+
+typedef struct _Set_elem_insert_log {
+    LogHdr         header;
+    SetElemInsData body;
+    char           *keyptr;
+    char           *valptr;
+} SetElemInsLog;
+
+/* Set Elem Delete Log Record */
+typedef struct _Set_elem_delete_data {
+    uint16_t keylen;  /* key length */
+    uint16_t reserved_16[1];
+    uint32_t vallen;  /* value length */
+    char     data[1];
+} SetElemDelData;
+
+typedef struct _Set_elem_delete_log {
+    LogHdr         header;
+    SetElemDelData body;
+    char           *keyptr;
+    char           *valptr;
+} SetElemDelLog;
+
+/* Btree Elem Insert Log Record */
+typedef struct _Btree_elem_insert_data {
+    uint16_t keylen;  /* key length */
+    uint8_t  nbkey;   /* bkey length */
+    uint8_t  neflag;  /* eflag length */
+    uint32_t vallen;  /* value length */
+    char     data[1];
+} BtreeElemInsData;
+
+typedef struct _Btree_elem_insert_log {
+    LogHdr           header;
+    BtreeElemInsData body;
+    char             *keyptr;
+    char             *datptr;
+} BtreeElemInsLog;
+
+/* Btree Elem Delete Log Record */
+typedef struct _Btree_elem_delete_data {
+    uint16_t keylen;  /* key length */
+    uint8_t  nbkey;   /* bkey length */
+    char     data[1];
+} BtreeElemDelData;
+
+typedef struct _Btree_elem_delete_log {
+    LogHdr           header;
+    BtreeElemDelData body;
+    char             *keyptr;
+    char             *datptr;
+} BtreeElemDelLog;
+#endif
+
 /* Snapshot File Tail Record */
 typedef struct _snapshot_tail_log {
     LogHdr header;
 } SnapshotTailLog;
 
-/* Construct Log Record Function For Snapshot */
+/* Construct Log Record Functions */
 int lrec_construct_snapshot_head(LogRec *logrec);
 int lrec_construct_snapshot_tail(LogRec *logrec);
 int lrec_construct_snapshot_elem(LogRec *logrec, hash_item *it, void *elem);
@@ -191,6 +319,16 @@ int lrec_construct_link_item(LogRec *logrec, hash_item *it);
 int lrec_construct_unlink_item(LogRec *logrec, hash_item *it);
 int lrec_construct_flush_item(LogRec *logrec, const char *prefix, const int nprefix);
 int lrec_construct_setattr(LogRec *logrec, hash_item *it, uint8_t updtype);
+#ifdef ENABLE_PERSISTENCE_03_CMDLOG_COLL
+int lrec_construct_list_elem_insert(LogRec *logrec, hash_item *it, uint32_t totcnt, int eindex, list_elem_item *elem);
+int lrec_construct_list_elem_delete(LogRec *logrec, hash_item *it, uint32_t totcnt, int eindex, uint32_t delcnt);
+int lrec_construct_map_elem_insert(LogRec *logrec, hash_item *it, map_elem_item *elem);
+int lrec_construct_map_elem_delete(LogRec *logrec, hash_item *it, map_elem_item *elem);
+int lrec_construct_set_elem_insert(LogRec *logrec, hash_item *it, set_elem_item *elem);
+int lrec_construct_set_elem_delete(LogRec *logrec, hash_item *it, set_elem_item *elem);
+int lrec_construct_btree_elem_insert(LogRec *logrec, hash_item *it, btree_elem_item *elem);
+int lrec_construct_btree_elem_delete(LogRec *logrec, hash_item *it, btree_elem_item *elem);
+#endif
 
 void lrec_write_to_buffer(LogRec *logrec, char *bufptr);
 #endif
