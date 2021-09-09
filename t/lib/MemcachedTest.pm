b05e1d7585a9d6a4dc04c599e19522e8c11c3774
suhwanJang <baseball9185@naver.com>
2021-09-09 18:43:21 +0900

    TEST: add keyscan test

--- t/lib/MemcachedTest.pm
+++ t/lib/MemcachedTest.pm
@@ -16,7 +16,7 @@ my $builddir = getcwd;
              mem_get_is mem_gets mem_gets_is mem_stats mem_cmd_val_is mem_cmd_is
              getattr_is lop_get_is sop_get_is mop_get_is bop_get_is bop_gbp_is bop_pwg_is bop_smget_is
              bop_ext_get_is bop_ext_smget_is bop_new_smget_is bop_old_smget_is
-             stats_prefixes_is stats_noprefix_is stats_prefix_is
+             stats_prefixes_is stats_noprefix_is stats_prefix_is keyscan
              supports_sasl free_port);
 
 sub sleep {
@@ -1013,6 +1013,34 @@ sub stats_prefix_is {
     }
     my $response = join(" ", @res_array);
     Test::More::is($response, $expected, $msg);
+}
+
+#KEYSCAN
+sub keyscan {
+    my ($sock_opts, $cursor, $count, $type, $pattern) = @_;
+    my $opts = ref $sock_opts eq "HASH" ? $sock_opts : {};
+    my $sock = ref $sock_opts eq "HASH" ? $opts->{sock} : $sock_opts;
+
+    my $response;
+    my @get_keys;
+    while (1) {
+        print $sock "keyscan $cursor count $count type $type match $pattern\r\n";
+        $response = <$sock>;
+        Test::More::like($response, qr/^KEYS/);
+        $response =~ /KEYS (\d+) (\d+)/;
+        $cursor = $2;
+        while (1) {
+            $_ = <$sock>;
+            if ($_ =~ /^END/) {
+                last;
+            }
+            push(@get_keys, (substr $_, 0, length($_)-2));
+        }
+        if ($cursor eq "0") {
+            last;
+        }
+    }
+    return @get_keys;
 }
 
 sub free_port {
