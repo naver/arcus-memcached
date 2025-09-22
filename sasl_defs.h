d341e18832ff1a16adc40916abdf194be6aa3605
Namjae Kim <namjae.kim@namsic.dev>
2025-09-22 12:29:47 +0000

    FIX: Use dummy arcus_sasl_authz() when sasl is disabled

--- sasl_defs.h
+++ sasl_defs.h
@@ -4,25 +4,28 @@
 // Longest one I could find was ``9798-U-RSA-SHA1-ENC''
 #define MAX_SASL_MECH_LEN 32
 
-uint16_t arcus_sasl_authz(const char *username);
 
 #if defined(ENABLE_SASL)
 
 #include <sasl/sasl.h>
 int init_sasl(void);
 void shutdown_sasl(void);
+uint16_t arcus_sasl_authz(const char *username);
 
 #elif defined(ENABLE_ISASL)
 
 #include "isasl.h"
 int init_sasl(void);
 void shutdown_sasl(void);
+uint16_t arcus_sasl_authz(const char *username);
+
 #else /* End of SASL support */
 
 typedef void* sasl_conn_t;
 
 #define shutdown_sasl()
 #define init_sasl() 0
+#define arcus_sasl_authz(a) 0
 #define sasl_dispose(x) {}
 #define sasl_server_new(a, b, c, d, e, f, g, h) 1
 #define sasl_listmech(a, b, c, d, e, f, g, h) 1
