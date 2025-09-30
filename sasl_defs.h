2d01611b8bef5862019a9423ce04c7ad8dab7284
Namjae Kim <namjae.kim@namsic.dev>
2025-09-30 08:18:16 +0000

    CLEANUP: Cleanup sasl-related stats

--- sasl_defs.h
+++ sasl_defs.h
@@ -11,6 +11,7 @@
 int init_sasl(void);
 void shutdown_sasl(void);
 uint16_t arcus_sasl_authz(const char *username);
+const char *sasl_engine_string(void);
 
 #if defined(ENABLE_ZK_INTEGRATION)
 void reload_sasl(void);
@@ -22,6 +23,7 @@ void reload_sasl(void);
 int init_sasl(void);
 void shutdown_sasl(void);
 uint16_t arcus_sasl_authz(const char *username);
+const char *sasl_engine_string(void);
 
 #else /* End of SASL support */
 
@@ -30,6 +32,7 @@ typedef void* sasl_conn_t;
 #define shutdown_sasl()
 #define init_sasl() 0
 #define arcus_sasl_authz(a) 0
+#define sasl_engine_string() "none"
 #define sasl_dispose(x) {}
 #define sasl_server_new(a, b, c, d, e, f, g, h) 1
 #define sasl_listmech(a, b, c, d, e, f, g, h) 1
