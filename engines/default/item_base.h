f877419a2cebad86d496abf108bcafb40f71f76b
zhy2on <dhwlgus5940@gmail.com>
2026-05-14 15:33:06 +0900

    INTERNAL: Clarify elem_item comments in item_base.h
    - nbytes/nfield: correct comments to reflect actual meaning
      (e.g. "total size of the data" → "size of the value")
    - remove redundant expressions such as "in data[]" and "(in bytes)"
    - unify /**< Doxygen-style comments to /*

--- engines/default/item_base.h
+++ engines/default/item_base.h
@@ -160,8 +160,8 @@ typedef struct _hash_item {
     rel_time_t time;    /* least recent access */
     rel_time_t exptime; /* When the item will expire (relative to process startup) */
     uint8_t  iflag;     /* Internal flags: item type and flag */
-    uint16_t nkey;      /* The total length of the key (in bytes) */
-    uint32_t nbytes;    /* The total length of the data (in bytes) */
+    uint16_t nkey;      /* The total length of the key */
+    uint32_t nbytes;    /* The total length of the data */
     /* Following fields are used to trade off memory space for performance */
     uint32_t khash;     /* The hash value of key string */
     void    *pfxptr;    /* pointer to prefix structure */
@@ -173,10 +173,10 @@ typedef struct _list_elem_item {
     uint8_t  slabs_clsid;         /* which slab class we're in */
     uint8_t  status;              /* element lifecycle state: linked(in-list) or unlinked(removed but referenced) */
     uint16_t reserved;            /* reserved space */
-    uint16_t nbytes;              /**< The total size of the data (in bytes) */
+    uint16_t nbytes;              /* the size of the value */
     struct _list_elem_item *next; /* next chain in double linked list */
     struct _list_elem_item *prev; /* prev chain in double linked list */
-    char     value[];             /**< the data itself */
+    char     value[];             /* the data itself */
 } list_elem_item;
 
 /* set element */
@@ -185,10 +185,10 @@ typedef struct _set_elem_item {
     uint8_t  slabs_clsid;         /* which slab class we're in */
     uint8_t  status;              /* element lifecycle state: linked(in-set) or unlinked(removed but referenced) */
     uint16_t reserved;            /* reserved space */
-    uint16_t nbytes;              /**< The total size of the data (in bytes) */
+    uint16_t nbytes;              /* the size of the value */
     struct _set_elem_item *next;  /* hash chain next */
     uint32_t hval;                /* hash value */
-    char     value[];             /**< the data itself */
+    char     value[];             /* the data itself */
 } set_elem_item;
 
 /* map element */
@@ -196,9 +196,9 @@ typedef struct _map_elem_item {
     uint16_t refcount;
     uint8_t  slabs_clsid;         /* which slab class we're in */
     uint8_t  status;              /* element lifecycle state: linked(in-map) or unlinked(removed but referenced) */
-    uint8_t  nfield;              /**< The total size of the field (in bytes) */
+    uint8_t  nfield;              /* the size of the field */
     uint8_t  reserved;            /* reserved space */
-    uint16_t nbytes;              /**< The total size of the data (in bytes) */
+    uint16_t nbytes;              /* the size of the value */
     struct _map_elem_item *next;  /* hash chain next */
     uint32_t hval;                /* hash value */
     unsigned char data[];         /* data: <field, value> */
@@ -209,9 +209,9 @@ typedef struct _btree_elem_item {
     uint16_t refcount;
     uint8_t  slabs_clsid;        /* which slab class we're in */
     uint8_t  status;             /* element lifecycle state: linked(in-tree) or unlinked(removed but referenced) */
-    uint8_t  nbkey;              /* length of bkey */
-    uint8_t  neflag;             /* length of element flag */
-    uint16_t nbytes;             /**< The total size of the data (in bytes) */
+    uint8_t  nbkey;              /* the size of the bkey */
+    uint8_t  neflag;             /* the size of the element flag */
+    uint16_t nbytes;             /* the size of the value */
     unsigned char data[];        /* data: <bkey, [eflag,] value> */
 } btree_elem_item;
 
