60491219d3a66531013bcce7372c17b1f925a589
Namjae Kim <namjae.kim@namsic.dev>
2026-06-29 08:26:24 +0000

    FIX: Revert cookie parameter to item_store

--- engines/default/default_engine.c
+++ engines/default/default_engine.c
@@ -415,7 +415,7 @@ default_store(ENGINE_HANDLE* handle, const void *cookie,
     VBUCKET_GUARD(engine, vbucket);
 
     ACTION_BEFORE_WRITE(cookie, item_get_key(it), it->nkey);
-    ret = item_store(it, cas, operation);
+    ret = item_store(it, cas, operation, cookie);
     ACTION_AFTER_WRITE(cookie, engine, ret);
     return ret;
 }
